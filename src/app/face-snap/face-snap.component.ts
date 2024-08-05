bb0284cf11218394bf2260e1ab4c0c9e7ea7da79
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-05 04:58:14 +0200

    added:directive  [ngStyle]

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
