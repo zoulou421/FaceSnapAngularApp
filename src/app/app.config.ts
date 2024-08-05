6764a75f67b44ad773f8161fc5fa9829d0f5263a
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-05 06:23:48 +0200

    added:Locale fr-FR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    {provide:LOCALE_ID,useValue:'fr-FR'}
+  ]
 };
