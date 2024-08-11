66c46eee3912fbc52e1272e3fd807e6b701bebcd
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-11 12:15:11 +0200

    ng g c landing-page//routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-    {path:'facesnaps',component:FaceSnapListComponent}
+    {path:'facesnaps',component:FaceSnapListComponent},
+    {path:'',component:LandingPageComponent}
 ];
